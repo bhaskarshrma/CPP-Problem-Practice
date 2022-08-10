19812ef6c0de6b9ae8ced1138ad8af4b959ae9c4
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-08-11 01:32:14 +0530

    first commit of this repo

---
+++ random_num_print_interval.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c,num=0;
+    for(c=0;c<5000;c++)
+    {
+        num+=1;
+        if(num%3==0 && num%5==0)
+        {
+             cout << "FizzBuzz" << endl;
+        }
+         else
+           { if(num%3==0)
+               {
+				   cout << "Fizz" << endl;
+				}
+				else
+				{
+					if(num%5==0)
+					{
+						cout << "Buzz" << endl;
+					}
+						else
+							{
+								cout << num << endl;
+							}
+				}
+			}
+	}
+	//return 0;
+}
