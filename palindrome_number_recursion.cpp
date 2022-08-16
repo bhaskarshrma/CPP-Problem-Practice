54ed5765240a84c32ccc618a9d81b7eb900b58b7
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-08-16 23:30:47 +0530

    adding a commit on file palindrome_number_recursion.cpp

---
+++ palindrome_number_recursion.cpp
@@ -0,0 +1,31 @@
+
+// Recursive C++ program to check if the number is palindrome or not 
+#include <iostream> 
+using namespace std; 
+ 
+int rev(int n, int temp) 
+{ 
+    if (n == 0) 
+      return temp; 
+		 
+	else
+      {
+	    temp = (temp * 10) + (n);  // stores the reverse of a number
+        return rev(n/10); //recursive call 
+      }
+}
+
+
+int main()
+{   
+    int n,temp;
+	cout<<"Enter a number:\n";
+	cin>>n;
+    temp = rev(n, 0);   //function call
+    if (temp == n)
+        cout << "\nYes number is a Palindrome" ;
+    else
+        cout << "\nNo number is not a Palindrome" ;
+    return 0;
+} 
+
