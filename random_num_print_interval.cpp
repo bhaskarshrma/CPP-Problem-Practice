3c4902fd49a83f2acebe4651b607cb76a374f154
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-08-19 00:14:33 +0530

    cin and cout added for user input/output

--- random_num_print_interval.cpp
+++ random_num_print_interval.cpp
@@ -1,19 +1,24 @@
-#include <iostream>
+# CPP program to print random numbers between a particular range
 
+#include <iostream>
 using namespace std;
 
 int main()
 {
 	int i, rnum, amount, first, last;
-	
-    	first=100,last=50000;
-    	amount=500;
-    	cout<<"\nLst of "<<amount<<" Random Numbers Between "<<first<<" and "<<last<<endl<<endl;
-    	srand(time(0));
-    	for(i=0; i<amount; i++)
-   	 {	
-       		rnum = (rand()%(last-first+1)) + first;
-        	cout<<i+1<<"). "<<"\t"<<rnum<<endl;
-   	 }
-    	cout<<endl;
+    cout<<"Enter the Interval: ";
+    //first=100,last=5000;
+    cin>>first>>last;
+    cout<<"How many Random Numbers to Generate ? ";
+    cin>>amount;
+    //amount=500;
+    cout<<"\nLst of "<<amount<<" Random Numbers Between "<<first<<" and "<<last<<endl<<endl;
+    srand(time(0));
+    for(i=0; i<amount; i++)
+    {
+       rnum = (rand()%(last-first+1)) + first;
+        cout<<i+1<<"). "<<"\t"<<rnum<<endl;
+    }
+    cout<<endl;
 }
+
