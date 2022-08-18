d555cd7f5f2d01eb9724e8f2ab13b8517050c139
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-08-18 21:34:58 +0530

    first commit on file call_by_refrence

---
+++ call_by_refrence.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+void incrementPassByReference(int var)
+{
+        var++;
+}
+
+int main()
+{
+        int i = 10;
+        cout << "Value of i before increment is : " << i << endl;
+        incrementPassByReference(i);
+        cout<< "Value of i after increment is : " << i << endl;
+}
+
