cef930afd13a4ca5de2c91d6ca6ee41900a39b9d
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-08-21 12:05:26 +0530

    2nd commit to add header file <cstring>

--- reverse_string_using_for.cpp
+++ reverse_string_using_for.cpp
@@ -1,10 +1,15 @@
+// CPP program to reverse a string with for loop
+
 #include <iostream>
+#include <cstring>
+
 using namespace std;
-void revStr(string str)
+void revStr(string str) // Function to reverse a string
 {
     for (int i = str.length() - 1; i >= 0; i--)
         cout << str[i];
 }
+
 int main(void)
 {
     string s = "Happy-Birthday";
