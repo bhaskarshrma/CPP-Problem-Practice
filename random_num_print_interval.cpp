fcb2dd1ed692e41f64b845a61bbf5d013635d09a
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-08-24 23:59:38 +0530

    2 sloc changes

--- random_num_print_interval.cpp
+++ random_num_print_interval.cpp
@@ -7,13 +7,12 @@ int main()
 {
 	int i, rnum, amount, first, last;
     cout<<"Enter the Interval: ";
-    //first=100,last=5000;
     cin>>first>>last;
     cout<<"How many Random Numbers to Generate ? ";
     cin>>amount;
-    //amount=500;
-    cout<<"\nLst of "<<amount<<" Random Numbers Between "<<first<<" and "<<last<<endl<<endl;
+    cout<<"\nList of "<<amount<<" Random Numbers Between "<<first<<" and "<<last<<endl<<endl;
     srand(time(0));
+	
     for(i=0; i<amount; i++)
     {
        rnum = (rand()%(last-first+1)) + first;
