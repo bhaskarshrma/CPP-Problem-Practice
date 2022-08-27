ecf18c9daf1b37feedaa2eae6ddc2022fd6067d5
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-08-28 00:24:12 +0530

    cpp program for struct- first commit

---
+++ struct-in-cpp.cpp
@@ -0,0 +1,35 @@
+// CPP program to show the use of CPP struct
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct movieDetails 
+{
+  string title;
+  int year;
+} mine, yours;
+
+
+int main() 
+{
+  mine.title = "Harry Potter and the Philosopher's Stone";
+  mine.year = 2001;
+
+  cout << "Enter title: ";
+  getline(cin, yours.title);
+  cout << "Enter year: ";
+  cin >> yours.year;
+
+  cout << "My favorite movie is:\n ";
+  printmovie(mine);
+  cout << "And yours is:\n ";
+  printmovie(yours);
+  
+}
+
+void printmovie(movieDetails movie) {
+  cout << movie.title;
+  cout << " (" << movie.year << ")\n";
+}
