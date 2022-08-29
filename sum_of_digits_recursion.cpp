eddae6b6df1549af10011f3402dbca9d69d3c439
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-08-29 23:57:13 +0530

    first commit on file sum-of-digits-rec

---
+++ sum_of_digits_recursion.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream> 
+using namespace std;
+
+int sum(int n) 
+{ 
+    if (n == 0) 
+       return 0; 
+    else
+	   return (n % 10 + sum( n / 10 )); 
+} 
+
+int main()
+{ 
+    int num, result;
+    cout<< "Enter a number:  \n";
+    cin>>num;
+    result = sum(num);
+    cout<<"Sum of all digits is: \n"<<result;
+} 
