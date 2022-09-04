493ffa7cb1e96fb57d4e0255611623f4844ae630
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-09-04 12:22:34 +0530

    cpp file to find whether a number is prime or not

---
+++ prime-number-using-for.cpp
@@ -0,0 +1,31 @@
+// CPP program to check prime number using for loop
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int num, count=0;
+  cout << "Enter a Positive Integer: ";
+  cin >> num;
+
+  // check for negative numbers and 1
+  if(num<=1) 
+  {count++;}
+  
+  // check for prime
+  for (int i=2; i <= num/2; i++)
+  {
+	  if (num % i == 0)
+	   {
+         count++;
+         break;
+		}
+  }
+
+  // display result
+  if (count == 0)
+     cout << "Prime Number." << endl;
+  else
+     cout << "Not a Prime Number." << endl;
+}
