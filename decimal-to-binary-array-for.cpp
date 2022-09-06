6bb683868fa847ff7f22127c98d321de30fb284f
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-09-06 17:34:17 +0530

    decimal-to-binary-array-for.cpp

--- decimal-to-binary-array-for.cpp
+++ decimal-to-binary-array-for.cpp
@@ -1,19 +1,21 @@
+//CPP program to print binary equivalent of entered decimal number using for array
+
 #include <iostream>  
 using namespace std; 
- 
+
 int main()  
 {  
-int a[20], n, i;    
-cout<<"Enter the number to convert: ";    
-cin>>n;    
-for(i=0; n>0; i++)    
-{    
-a[i]=n%2;    
-n= n/2;  
-}    
-cout<<"Binary of the given number= ";    
-for(i=i-1 ;i>=0 ;i--)    
-{    
-cout<<a[i];    
-}    
+    int A[20], n, i;    
+    cout<<"Enter the number to convert: ";    
+    cin>>n;    
+    for(i=0; n>0; i++)    // loop to calculate binary equivalent
+      {    
+        A[i]=n%2;
+        n= n/2;
+      }
+    cout<<"Binary of the given number= ";    // loop to print binary
+    for(i=i-1 ;i>=0 ;i--)    
+      {
+        cout<<A[i];    
+      }
 }
