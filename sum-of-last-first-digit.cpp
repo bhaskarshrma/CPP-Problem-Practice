e492e9a891384d2493f33f20fc5320332659fcfd
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-09-09 17:56:08 +0530

    comments added for program

--- sum-of-last-first-digit.cpp
+++ sum-of-last-first-digit.cpp
@@ -1,5 +1,8 @@
+// CPP program to find Sum of First and Last Digit using for Loop
 
-#include<iostream>
+// Since for loop also contains initialize and update statements along with condition checking. Therefore, just place any initialization say temp=0 or sum=0 and the update statement as num = num/10
+
+#include <iostream>
 
 using namespace std;
 int main()
