ee97a77316db0a1928aca6263a1a41f56ba6bd48
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-09-10 22:56:39 +0530

    comments in the code added second commit

--- armstrong-while-loop.cpp
+++ armstrong-while-loop.cpp
@@ -1,3 +1,7 @@
+//CPP program to check whether the number is Armstrong or not.
+//Armstrong number is a number that is equal to the sum of cubes of its digits.
+//For example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers.
+
 
 #include <iostream>
 using namespace std;  
