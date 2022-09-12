fdb4b38bae687eb4fdd354d2fc6d72216ce2944b
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-09-12 23:45:12 +0530

    function with pointers in cpp, 2nd commit

--- functions-with-pointers.cpp
+++ functions-with-pointers.cpp
@@ -1,8 +1,14 @@
+// CPP program to call the function via the function pointer 
+
+// We define the function printname() in the preceding program, which takes a char pointer as an argument. We declared our function pointer as void (*ptr)(char*).
+
+//We are setting the address of the printname() function to ptr with the expression ptr=printname. We can now use the ptr statement to call the printname() methods. 
+
 #include <iostream>  
 using namespace std;  
 void printname(char *name)  
 {  
-    std::cout << "Name:" <<name<< std::endl;  
+    cout << "Name:" <<name<<endl;  
 }  
   
 int main()  
@@ -10,9 +16,9 @@ int main()
     char x[30];  // array declaration  
     void (*ptr)(char*);  // function pointer declaration  
     ptr=printname;  // storing the address of printname in ptr.  
-    std::cout << "Enter name: " << std::endl;  
+    cout << "Enter name: " << endl;  
     cin>>x;  
     cout<<x<<endl;  
     ptr(x);  // calling printname() function  
-   return 0;  
+      
 }
