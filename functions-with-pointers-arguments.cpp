ff99aaad24668641992c171e54872ebd9922d1ec
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-09-12 23:49:16 +0530

    functions-with-pointers-arguments.cpp 2nd commit

--- functions-with-pointers-arguments.cpp
+++ functions-with-pointers-arguments.cpp
@@ -1,3 +1,7 @@
+// have passed a function pointer as an argument to the function2() function. 
+// The address of function1() is provided to the func2() function by the main() method. 
+// The function2() function is indirectly invoking the function1() function in this manner.
+
 #include <iostream>  
 using namespace std;  
 void function1()  
