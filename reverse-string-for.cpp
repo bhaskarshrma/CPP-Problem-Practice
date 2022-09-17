d915895ae08130af17a335829f22505b18107026
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-09-17 22:53:30 +0530

    second commit on this file

--- reverse-string-for.cpp
+++ reverse-string-for.cpp
@@ -1,3 +1,5 @@
+// CPP program to reverse a string with for loop
+
 #include<iostream>
 using namespace std;
 
@@ -5,8 +7,8 @@ int main()
 {
   string str, strRev;
   cout<<"Enter a string : "<<endl;
-  getline(cin,str);
-  
+  getline(cin,str);  //using getline() to take input
+
   for(int i=str.size()-1;i>=0;i--)  // reversing the 'str' string
    {
      strRev.push_back(str[i]);
