9af0951e9c95c744c56df849e4334c24cb650a41
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-09-21 23:59:51 +0530

    first commit on file

---
+++ number-guessing-game - Copy.cpp
@@ -0,0 +1,25 @@
+// CPP program to help user guess a number which computer assumed already
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{  
+  srand(time(NULL));
+  int secNum=rand() % 25, guess=0;
+
+  do 
+  {
+    cout<<"Guess a number : ";
+    cin>>guess;
+    if(guess>secNum)
+     cout<<"too Big\n";
+    if(guess<secNum)
+     cout<<"too small\n";
+     
+  } while (secNum!=guess);
+
+  cout<<"You got it!!! "<<endl;
+
+}
