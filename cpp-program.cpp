55dcc6c6d685c316386cb06fa6ebe516ebdf6dd8
bhaskarshrma <110971795+bhaskarshrma@users.noreply.github.com>
2022-10-13 00:00:56 +0530

    new file - first commit

---
+++ cpp-program.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+
+class GradeBook
+{
+    public:
+    void setCourseName(string name)
+    {
+        courseName = name;
+    }
+
+    string getCourseName() const
+    {
+        return courseName;
+    }
+
+    void displayMsg() const
+    {
+        cout << "Welcome to GradeBook for : " << getCourseName() << endl;
+    }
+    
+    private:
+    string courseName = "Computer Application";
+};
+
+int main()
+{
+    string nameofCourse;
+    GradeBook myGradeBook;
+    cout << "Initial course name is : " << myGradeBook.getCourseName() << endl;
+    cout << "Enter the course name : ";
+    getline(cin, nameofCourse);
+    myGradeBook.setCourseName(nameofCourse);
+    cout << endl;
+    myGradeBook.displayMsg();
+
+}
